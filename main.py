bdc3d4a6e139be77020be8f95e887e04c9d48b79
S48avio <151058808+S48avio@users.noreply.github.com>
2024-05-20 13:19:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,30 +2,29 @@ import torch
 import transformers
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
+# notebook_login()
 from huggingface_hub.hf_api import HfFolder
 HfFolder.save_token('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')
 
 tokenizer = AutoTokenizer.from_pretrained("arya123321/arya",
                                           token= "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",)
 
-print("before auto causal")
 model = AutoModelForCausalLM.from_pretrained("arya123321/arya",
                                              device_map='auto',
-                                             
                                              torch_dtype=torch.float16,
                                              token="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",
-                                             offload_folder  = "./offload"
                                             #  load_in_8bit=True,
                                             #load_in_4bit=True
                                              )
-print("After auto causal")
+
+from transformers import pipeline
 
 import json
 import textwrap
 
 B_INST, E_INST = "[INST]", "[/INST]"
 B_SYS, E_SYS = "<<SYS>>\n", "\n<</SYS>>\n\n"
-DEFAULT_SYSTEM_PROMPT = """Generate a cooking recipe using the given ingredients.List all ingredients and steps in detail."""
+DEFAULT_SYSTEM_PROMPT = """\Generate a cooking recipe using the given ingredients.List all ingredients and steps in detail."""
 
 SYSTEM_PROMPT = B_SYS + DEFAULT_SYSTEM_PROMPT + E_SYS
 
@@ -49,20 +48,16 @@ def remove_substring(string, substring):
 
 def generate(text):
     prompt = get_prompt(text)
-    print("after get_prompt()")
-    with torch.autocast('cpu', dtype=torch.bfloat16):
-        inputs = tokenizer(prompt, return_tensors="pt")#.to('cuda')
-        print("after inputs")
+    with torch.autocast('cuda', dtype=torch.bfloat16):
+        inputs = tokenizer(prompt, return_tensors="pt").to('cuda')
         outputs = model.generate(**inputs,
                                  max_new_tokens=512,
                                  eos_token_id=tokenizer.eos_token_id,
                                  pad_token_id=tokenizer.eos_token_id,
                                  )
-        print("After generate")
         final_outputs = tokenizer.batch_decode(outputs, skip_special_tokens=True)[0]
         final_outputs = cut_off_text(final_outputs, '</s>')
         final_outputs = remove_substring(final_outputs, prompt)
-    print("Reached end")
 
     return final_outputs#, outputs
 
@@ -70,9 +65,3 @@ def parse_text(text):
         wrapped_text = textwrap.fill(text, width=100)
         print(wrapped_text +'\n\n')
         # return assistant_text
-
-prompt = 'bread,egg,milk'
-print("Before generate()")
-generated_text = generate(prompt)
-print(f"geerated text: {generated_text}")
-parse_text(generated_text)
